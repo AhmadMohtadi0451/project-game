363a20efe5d219ab2da39bec581bcf2741d119b3
Ahmad Mohtadi <ahmadmohtadi0451@gmail.com>
2023-07-05 22:34:21 +0330

    Handle Errors

--- src/Routes/router.tsx
+++ src/Routes/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Layout from "../Pages/Layout";
 import HomePage from "../Pages/HomePage";
 import GameDetailPage from "../Pages/GameDetailPage";
+import ErrorPage from "../Pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
@@ -11,6 +12,7 @@ const router = createBrowserRouter([
       { path: "", element: <HomePage /> },
       { path: "games/:id", element: <GameDetailPage /> },
     ],
+    errorElement: <ErrorPage />,
   },
 ]);
 
